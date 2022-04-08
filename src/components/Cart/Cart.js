e2233f62c644cd9f47416cc9f04f36bcf82501b7
Nicolas Arcieri <nicolasarcieri@gmail.com>
2022-04-08 14:11:34 -0300

    context con descuento

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -6,10 +6,7 @@ const Cart = () => {
 
     const { cart, clearCart, getPrice, removeItem} = useContext(CartContext)
 
-    // const [precioFinal, setPrecioFinal] = useState(getPrice()) modifico esta linea por la de abajo
-    let precioFinal = getPrice(); //por esta
-    const [descuento, setDescuento] = useState('');
-    let canjeoCodigo = false;  
+    const [canjeoCodigo, setCanjeoCodigo] = useState(false)
     const [codigo, setCodigo] = useState('')
 
     const canjear = (valor) => {
@@ -17,10 +14,7 @@ const Cart = () => {
             console.log('Ya lo canjeo')
         } else {
             if (valor === 'Comision31855') {
-                // setPrecioFinal(0.9*getPrice()); Muestra correctamente el precio con el descuento aplicado, pero deja de funcionar con el boton de eliminar item o vaciar carro
-                setDescuento('-10% (-'+ getPrice()/10 + ' USD)');
-                precioFinal = precioFinal-getPrice()/10
-                canjeoCodigo = true;
+                setCanjeoCodigo(true);
                 console.log('codigo canjeado')
             } else {
                 console.log('codigo invalido')
@@ -38,8 +32,10 @@ const Cart = () => {
                     <tbody>
                     {cart.map(prod => <tr key={prod.id}><td><b>{prod.name}</b> x {prod.quantity}</td><td>{prod.price*prod.quantity} USD ({prod.price} USD c/u)</td><td><button className="btn btn-secondary btn-block" onClick={() => {removeItem(prod.id)}}> Eliminar un producto</button></td></tr>)}
                     <tr><td></td><td></td><td></td></tr>
-                    <tr><td><input type="text" name="name" placeholder="Codigo de Descuento" onChange={e => setCodigo(e.target.value)} /></td><td>{descuento}</td><td><button className="btn btn-secondary btn-block" onClick={() => canjear(codigo)}>Canjear</button></td></tr>
-                    <tr><th>Total a pagar</th><th>{precioFinal} USD</th><th><button className="btn btn-secondary btn-block" onClick={clearCart}>Vaciar carrito</button></th></tr>
+                    <tr><td><input type="text" name="name" placeholder="Codigo de Descuento" onChange={e => setCodigo(e.target.value)} /></td>{canjeoCodigo ? <td>-{getPrice()/10} USD (-10%)</td> : <td></td>}<td><button className="btn btn-secondary btn-block" onClick={() => canjear(codigo)}>Canjear</button></td></tr>
+                    <tr><th>Total a pagar</th>
+                    {canjeoCodigo ? <th>{0.9*getPrice()} USD</th> : <th>{getPrice()} USD</th>}
+                    <th><button className="btn btn-secondary btn-block" onClick={clearCart}>Vaciar carrito</button></th></tr>
                     </tbody>
                 </table>  
                 <button className="btn btn-secondary btn-block">Pagar</button>
